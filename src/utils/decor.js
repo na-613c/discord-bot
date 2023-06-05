10e1616289926445af2cc1f6a98e104830f695eb
IVAN <na-613c@github.com>
2023-06-05 20:52:33 +0300

    Fixed decor

--- src/utils/decor.js
+++ src/utils/decor.js
@@ -6,12 +6,12 @@ async function getDecorResponse(resources) {
 
     return [
         {
-            sell: 0.9 * (40 * metal100.sell + 60 * copper100.sell + 100 * wires100.sell + 30 * plastic100.sell),
-            buy: 0.9 * (40 * metal100.buy + 60 * copper100.buy + 100 * wires100.buy + 30 * plastic100.buy),
+            sell: 0.9 * (40 * metal100.sell + 100 * copper100.sell + 60 * wires100.sell + 30 * plastic100.sell),
+            buy: 0.9 * (40 * metal100.buy + 100 * copper100.buy + 60 * wires100.buy + 30 * plastic100.buy),
         },
         {
-            sell: 0.9 * (80 * metal100.sell + 170 * copper100.sell + 150 * wires100.sell + 80 * plastic100.sell),
-            buy: 0.9 * (80 * metal100.buy + 170 * copper100.buy + 150 * wires100.buy + 80 * plastic100.buy),
+            sell: 0.9 * (80 * metal100.sell + 150 * copper100.sell + 170 * wires100.sell + 80 * plastic100.sell),
+            buy: 0.9 * (80 * metal100.buy + 150 * copper100.buy + 170 * wires100.buy + 80 * plastic100.buy),
         },
         {
             sell: 0.9 * (50 * metal100.sell + 250 * copper100.sell + 250 * batteries100.sell + 250 * electron100.sell),
